d7ee22949b57843a5ef00c72516eeebc1bcc28ed
j2nupark <wlsdn132323@gmail.com>
2022-01-28 03:15:03 +0900

    TIL init

---
+++ calaulater/calculater.js
@@ -0,0 +1,34 @@
+// let numbers = document.getElementsByClassName("digit")[0].innerHTML
+
+// let total = document.getElementById("total").innerHTML = numbers;
+
+// console.log(number);
+
+
+
+// let numOne = '';
+// let operator = '';
+// let numTwo = '';
+// const $operator = document.querySelector('#operator');
+// const total = document.querySelector('total');
+
+// const onClickNumber = (number) => () => {
+//     if (operator) {
+//       numTwo += number;
+//     } else {
+//       numOne += number;
+//     }
+//     console.log(numOne, numTwo);
+//     $result.value += number;
+//   }
+  
+//   document.querySelector('#num-0').addEventListener('click', onClickNumber('0'));
+//   document.querySelector('#num-1').addEventListener('click', onClickNumber('1'));
+//   document.querySelector('#num-2').addEventListener('click', onClickNumber('2'));
+//   document.querySelector('#num-3').addEventListener('click', onClickNumber('3'));
+//   document.querySelector('#num-4').addEventListener('click', onClickNumber('4'));
+//   document.querySelector('#num-5').addEventListener('click', onClickNumber('5'));
+//   document.querySelector('#num-6').addEventListener('click', onClickNumber('6'));
+//   document.querySelector('#num-7').addEventListener('click', onClickNumber('7'));
+//   document.querySelector('#num-8').addEventListener('click', onClickNumber('8'));
+//   document.querySelector('#num-9').addEventListener('click', onClickNumber('9'));
